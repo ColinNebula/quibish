fbe00076042a09bb74c6fa0239446cb03702b9e3
Quibish Developer <developer@quibish.app>
2025-09-17 09:29:27 -0400

    Updates

--- status.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-REM Check status of Quibish servers
-echo Checking Quibish Application status...
-echo.
-
-powershell -ExecutionPolicy Bypass -File "%~dp0start-servers.ps1" -Status
-
-echo.
-pause
